126d560834dc52e34643610e2a3ef4aaa62d3fa9
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-11-13 16:31:39 -0600

    some more setup cleanup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
- 
+
 setup(
     name = "django-admin-histograms",
     version = __import__("django_histograms").__version__,
@@ -10,6 +10,9 @@ setup(
     license = "BSD",
     url = "http://github.com/alex/django-admin-histograms",
     packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['django'],
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
