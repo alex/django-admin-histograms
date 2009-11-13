519bb853977f0ff9d60908fcd43f6bb12418ffd7
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-11-13 15:56:50 -0600

    Move histogram generation into generic Histogram class. Rendering is now handled by the class allowing generation outside of an admin panel.

--- django_histograms/admin.py
+++ django_histograms/admin.py
@@ -1,9 +1,5 @@
-import calendar
-import datetime
-
 from django.conf.urls.defaults import url, patterns
 from django.contrib import admin
-from django.db.models import Count
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -20,30 +16,13 @@ class HistogramAdmin(admin.ModelAdmin):
     
     def report_view(self, request):
         assert self.histogram_field is not None, "Set histogram_field you idiot"
-        this_month = datetime.date.today().replace(day=1)
-        last_month = (this_month - datetime.timedelta(days=1)).replace(day=1)
-        qs = self.queryset(request).values(self.histogram_field).annotate(
-            num=Count("pk")
-        ).filter(**{"%s__gt" % self.histogram_field: last_month})
+
+        histogram = Histogram(self.model, self.queryset(request))
         
-        months = [
-            (this_month, [0] * calendar.monthrange(this_month.year, this_month.month)[1]),
-            (last_month, [0] * calendar.monthrange(last_month.year, last_month.month)[1]),
-        ]
-        
-        for data in qs.iterator():
-            if (this_month.month == data[self.histogram_field].month and
-                this_month.year == data[self.histogram_field].year):
-                idx = 0
-            elif (last_month.month == data[self.histogram_field].month and
-                last_month.year == data[self.histogram_field].year):
-                idx = 1
-            else:
-                continue
-            months[idx][1][data[self.histogram_field].day-1] += data["num"]
-        
-        return render_to_response("admin/report.html", {
-            "results": months,
-            "total": sum(o for m in months for o in m[1]),
-            "title": "Histogram for %s" % self.model._meta.object_name,
-        }, context_instance=RequestContext(request, current_app=self.admin_site.name))
+        context = {
+            'title': "Histogram for %s" % self.model._meta.object_name,
+            'histogram': histogram,
+        }
+
+        return render_to_response("admin/report.html", context,
+            context_instance=RequestContext(request, current_app=self.admin_site.name))
---
+++ django_histograms/utils.py
@@ -0,0 +1,104 @@
+import calendar
+import datetime
+
+from django.utils.safestring import mark_safe
+from django.db.models import Count
+from django.shortcuts import render_to_string
+
+
+HISTOGRAM_CSS = """
+.histogram ul {
+    font-size: 0.75em;
+    height: 10em;
+  }
+
+.histogram li {
+    position: relative;
+    float: left;
+    width: 1.5em;
+    margin: 0 0.1em;
+    height: 8em;
+    list-style-type: none;
+}
+
+.histogram li a {
+    display: block;
+    height: 100%;
+}
+
+.histogram li .label {
+    display: block;
+    position: absolute;
+    bottom: -2em;
+    left: 0;
+    background: #fff;
+    width: 100%;
+    height: 2em;
+    line-height: 2em;
+    text-align: center;
+}
+
+.histogram li a .count {
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 0;
+    width: 100%;
+    background: #AAA;
+    text-indent: -9999px;
+    overflow: hidden;
+}
+
+.histogram li:hover {
+    background: #EFEFEF;
+}
+
+.histogram li a:hover .count {
+    background: #2D7BB2;
+}"""
+
+class Histogram(object):
+    def __init__(self, model, queryset=None):
+        # `queryset` exists so it can work with the admin (bad idea?)
+        self.model = model
+        self._queryset = None
+    
+    def render(self, attname):
+        context = self.get_report(attname)
+        
+        return render_to_string("histogram/report.html", context)
+        
+    def get_query_set(self):
+        return self._queryset or self.model.objects.all()
+    
+    def get_css(self):
+        return mark_safe(HISTOGRAM_CSS)
+    
+    def get_report(self, attname):
+        this_month = datetime.date.today().replace(day=1)
+        last_month = (this_month - datetime.timedelta(days=1)).replace(day=1)
+        qs = self.get_query_set().values(attname).annotate(
+            num=Count("pk")
+        ).filter(**{"%s__gt" % attname: last_month})
+        
+        months = [
+            (this_month, [0] * calendar.monthrange(this_month.year, this_month.month)[1]),
+            (last_month, [0] * calendar.monthrange(last_month.year, last_month.month)[1]),
+        ]
+        
+        for data in qs.iterator():
+            if (this_month.month == data[attname].month and
+                this_month.year == data[attname].year):
+                idx = 0
+            elif (last_month.month == data[attname].month and
+                last_month.year == data[attname].year):
+                idx = 1
+            else:
+                continue
+            months[idx][1][data[attname].day-1] += data["num"]
+        
+        return {
+            "results": months,
+            "total": sum(o for m in months for o in m[1]),
+        }
